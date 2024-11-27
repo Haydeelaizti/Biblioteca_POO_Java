6670a3c79b6cc3aabe0e88cd50f8b3c6d43f9cc2
Haydée Laiz <haydeelgp@gmail.com>
2024-11-26 21:47:54 -0300

    Implementação completa da lógica do menu no método main

--- src/biblioteca/SistemaBiblioteca.java
+++ src/biblioteca/SistemaBiblioteca.java
@@ -19,31 +19,49 @@ public class SistemaBiblioteca {
             System.out.println("7. Sair");
 
             int opcao = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Opção para adicionar livro selecionada.");
+                    System.out.println("Digite o título do livro:");
+                    String tituloLivro = scanner.nextLine();
+                    System.out.println("Digite o autor do livro:");
+                    String autorLivro = scanner.nextLine();
+                    sistema.adicionarItem(new Livro(tituloLivro, autorLivro));
+                    System.out.println("Livro adicionado com sucesso!");
                     break;
 
                 case 2:
-                    System.out.println("Opção para adicionar revista selecionada.");
+                    System.out.println("Digite o título da revista:");
+                    String tituloRevista = scanner.nextLine();
+                    System.out.println("Digite o autor da revista:");
+                    String autorRevista = scanner.nextLine();
+                    System.out.println("Digite a edição da revista:");
+                    int edicaoRevista = scanner.nextInt();
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    sistema.adicionarItem(new Revista(tituloRevista, autorRevista, edicaoRevista));
+                    System.out.println("Revista adicionada com sucesso!");
                     break;
 
                 case 3:
-                    System.out.println("Opção para adicionar usuário selecionada.");
+                    System.out.println("Digite o nome do usuário:");
+                    String nomeUsuario = scanner.nextLine();
+                    System.out.println("Digite a matrícula do usuário:");
+                    String matriculaUsuario = scanner.nextLine();
+                    sistema.adicionarUsuario(new Usuario(nomeUsuario, matriculaUsuario));
+                    System.out.println("Usuário adicionado com sucesso!");
                     break;
 
                 case 4:
-                    System.out.println("Opção para listar itens selecionada.");
+                    sistema.listarItens();
                     break;
 
                 case 5:
-                    System.out.println("Opção para listar usuários selecionada.");
+                    sistema.listarUsuarios();
                     break;
 
                 case 6:
-                    System.out.println("Opção para gerar relatório selecionada.");
+                    sistema.gerarRelatorio();
                     break;
 
                 case 7:
@@ -60,4 +78,3 @@ public class SistemaBiblioteca {
         scanner.close();
     }
 }
-
